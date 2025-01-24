d111851d84adc4ca874a85ae9f824072d80de8e2
Nikhilesh Joshi <joshinikhilesh46@gmail.com>
2025-01-25 03:04:54 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const URI = 'mongodb+srv://bydefaultstatic:careerCompass@cluster0.gzj1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const JwtSecret = 'secret';
+const B_PORT = 3300;
+
+module.exports = { URI, JwtSecret, B_PORT };
