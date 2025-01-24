4dcd0d045c8bf868a021d5b65751acada6b475f9
Nikhilesh Joshi <joshinikhilesh46@gmail.com>
2025-01-25 03:01:55 +0530

    Create auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const JwtSecret = process.env.JWT_SECRET;
+
+const auth = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const verified = jwt.verify(token, JwtSecret);
+    req.user_id = verified.id;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+};
+
+export default auth;
