35ca21cdb93b2254b8778011d34419a57b2ae488
Nikhilesh Joshi <joshinikhilesh46@gmail.com>
2025-01-25 03:06:27 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const URI = 'mongodb+srv://bydefaultstatic:careerCompass@cluster0.gzj1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-export const JwtSecret = 'secret';
-export const B_PORT = 3300;
+const URI = 'mongodb+srv://bydefaultstatic:careerCompass@cluster0.gzj1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const JwtSecret = 'secret';
+const B_PORT = 3300;
 
-
+module.exports = { URI, JwtSecret, B_PORT };
---
+++ index.js
@@ -0,0 +1,35 @@
+// index.js
+import express from 'express';
+import connectDB from './db/index.js';
+import authRouter from './routes/user.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production'
+    ? ['https://your-production-domain.com']
+    : ['http://localhost:5176'],
+  credentials: true,
+}));
+app.use(express.json());
+
+app.use('/auth', authRouter);
+
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
